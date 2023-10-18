cf61f230f9efd54be1c86aa048f040ef96fc076f
Muhamad Parhan <108636387+MochParhand@users.noreply.github.com>
2023-10-18 15:13:32 +0700

    Add files via upload

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,27 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  // bubleSort
+  for (let i = 0; i < result.length; i++) { 
+    // Last i elements are already in place   
+    for (let j = 0; j < (result.length - i - 1); j++) { 
+      // Checking if the item at present iteration  
+      // is greater than the next iteration 
+      if (result[j].year > result[j + 1].year) { 
+          // If the condition is true 
+          // then swap them 
+          let temp = result[j] 
+          result[j] = result[j + 1] 
+          result[j + 1] = temp 
+      } 
+    }  
+  } 
+  // Rubah code ini dengan array hasil sorting secara ascending
+  return result;
+}
